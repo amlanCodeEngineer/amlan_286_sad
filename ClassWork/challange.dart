c7cd1232cf0aa230ed8e306121d742e59263a8c9
Amlan Kumar Das <amlan.coder.ai@gmail.com>
2025-11-07 11:10:42 +0600

    Cleaned up old files, updated Assignment 5

--- ClassWork/challange.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  stdout.write("Enter your age: ");
-  int age = int.parse(stdin.readLineSync()!);
-
-  print((age >= 13 && age <= 19) ? "Teenager" : "Not Teenager");
-}
